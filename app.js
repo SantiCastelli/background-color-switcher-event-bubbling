78056d523d73bcd55519c02a6c2585a033a86a86
Santiago Castelli <castellist@gmail.com>
2022-03-24 10:15:50 +0100

    terminado

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // Happy coding!
 
+// document.querySelector(".canvas").addEventListener("click", function(event){
+//     console.log(event.target.id);
+//     document.body.style.backgroundColor = event.target.id;
+// }); // TERMINADO 
+
+let div = document.querySelector(".canvas");
+div.addEventListener("click", function(event){
+    console.log(window.getComputedStyle(event.target).backgroundColor);
+    
+    document.body.style.backgroundColor = getComputedStyle(event.target).backgroundColor;
+}); // BONUS
